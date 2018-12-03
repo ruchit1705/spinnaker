e9a2f336ec50157f0a50f7d183fb46e373f21542
ruchit1705 <ruchit_1705@yahoo.co.in>
2018-12-03 17:59:45 +0530

    changed success rate to 60

--- solutions/kayenta/app/app.py
+++ solutions/kayenta/app/app.py
@@ -8,8 +8,7 @@ import os
 app = Flask('kayenta-tester')
 c = Counter('requests', 'Number of requests served, by http code', ['http_code'])
 
-#os.environ['SUCCESS_RATE'] = 60
-os.environ['SUCCESS_RATE'] = 90
+os.environ['SUCCESS_RATE'] = 60
 
 @app.route('/')
 def hello():
